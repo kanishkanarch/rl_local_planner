0910d0f2bcea3adc57a0a72407906c81d40103b5
Kanishk <kanishk.vishwa2001@gmail.com>
2022-12-05 21:48:51 +0530

    Plugin working

--- src/rl_local_planner.cpp
+++ src/rl_local_planner.cpp
@@ -1,14 +1,26 @@
-#include <pluginlib/class_list_macros.h>
 #include "rl_local_planner.h"
+#include <pluginlib/class_list_macros.h>
+#include <tf/tf.h>
+#include <geometry_msgs/Twist.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <tf2_ros/buffer.h>
+#include <base_local_planner/costmap_model.h>
 
 PLUGINLIB_EXPORT_CLASS(rl_local_planner_ns::rl_local_planner, nav_core::BaseLocalPlanner)
 
 	namespace rl_local_planner_ns{
-		rl_local_planner::rl_local_planner(){}
+		rl_local_planner::rl_local_planner() 
+		{
+			initialize();
+		}
 
+               
+               rl_local_planner::~rl_local_planner() {}
 		bool rl_local_planner::computeVelocityCommands(geometry_msgs::Twist &cmd_vel)
 		{
-			cmd_vel.angular.z = 0.1;
+			cmd_vel.linear.x=0.1;
+			cmd_vel.angular.z=0.2;
+			return true;
 		}
 
 		void rl_local_planner::initialize(std::string name, tf2_ros::Buffer *tf, costmap_2d::Costmap2DROS *costmap_ros)
@@ -16,8 +28,11 @@ PLUGINLIB_EXPORT_CLASS(rl_local_planner_ns::rl_local_planner, nav_core::BaseLoca
 		}
 		bool rl_local_planner::isGoalReached()
 		{
+		  return false;
 		}
 		bool rl_local_planner::setPlan(const std::vector<geometry_msgs::PoseStamped> &plan)
-		{
+		{ return true;
 		}
+		
+		
 	};
--- src/rl_local_planner.h
+++ src/rl_local_planner.h
@@ -1,3 +1,9 @@
+#ifndef RL_LOCAL_PLANNER_H
+#define RL_LOCAL_PLANNER_H
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+#include <std_srvs/Empty.h>
 #include <nav_core/base_local_planner.h>
 #include <pluginlib/class_list_macros.h>
 #include <costmap_2d/costmap_2d_ros.h>
@@ -13,11 +19,19 @@ namespace rl_local_planner_ns{
 		public:
 			rl_local_planner();
 			~rl_local_planner();
+			ros::NodeHandle nh;
+			ros::Subscriber odom_sub;
+			ros::Subscriber scan_sub;
+			ros::ServiceClient respawner;
+			nav_msgs::Odometry odom;
+			sensor_msgs::LaserScan scan;
+			void odom_callback(const nav_msgs::Odometry odom_msg);
+			void scan_callback(const sensor_msgs::LaserScan scan_msg);
 			bool computeVelocityCommands(geometry_msgs::Twist &cmd_vel);
 			void initialize(std::string name, tf2_ros::Buffer *tf, costmap_2d::Costmap2DROS *costmap_ros);
 			bool isGoalReached();
 			bool setPlan(const std::vector<geometry_msgs::PoseStamped> &plan);
 	};
 };
-
-PLUGINLIB_EXPORT_CLASS(rl_local_planner_ns::rl_local_planner, nav_core::BaseLocalPlanner)
+#endif
+//PLUGINLIB_EXPORT_CLASS(rl_local_planner_ns::rl_local_planner, nav_core::BaseLocalPlanner)
